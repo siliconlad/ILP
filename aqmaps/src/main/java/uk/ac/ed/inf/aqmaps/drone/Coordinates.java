c926d783df3b53f102102b471890d80d7cbc16f1
Siliconlad <siliconlad@protonmail.com>
2020-11-29 01:18:29 +0000

    Override equality method for Coordinates class

--- aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/drone/Coordinates.java
+++ aqmaps/src/main/java/uk/ac/ed/inf/aqmaps/drone/Coordinates.java
@@ -8,4 +8,17 @@ public class Coordinates {
   public String toString() {
     return Double.toString(this.lng) + "," + Double.toString(this.lat);
   }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Coordinates)) {
+      return false;
+    }
+    
+    Coordinates otherObj = (Coordinates)obj;
+    var lngsEqual = Double.compare(this.lng, otherObj.lng);
+    var latsEqual = Double.compare(this.lat, otherObj.lat);
+    
+    return (lngsEqual == 0) && (latsEqual == 0);
+  }
 }
