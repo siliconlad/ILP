082f1ffa7b49495759d958b186b30a6ac9abde61
Siliconlad <siliconlad@protonmail.com>
2020-11-30 04:47:20 +0000

    Remove unused import

--- aqmaps/src/test/java/uk/ac/ed/inf/aqmaps/webserver/WebServerTest.java
+++ aqmaps/src/test/java/uk/ac/ed/inf/aqmaps/webserver/WebServerTest.java
@@ -3,7 +3,6 @@ package uk.ac.ed.inf.aqmaps.webserver;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
